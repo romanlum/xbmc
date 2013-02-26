e6cd1044e27763af2f8e8a1334e1df0eb5d6c80b
Garrett Brown <garbearucla@gmail.com>
2013-02-26 06:32:27 -0800

    Add default icon for game files.

--- xbmc/FileItem.cpp
+++ xbmc/FileItem.cpp
@@ -1273,6 +1273,7 @@ void CFileItem::FillInDefaultIcon()
   //   default picture for photo's
   //   default picture for songs
   //   default picture for videos
+  //   default picture for games
   //   default picture for shortcuts
   //   default picture for playlists
   //   or the icon embedded in an .xbe
@@ -1336,6 +1337,10 @@ void CFileItem::FillInDefaultIcon()
       else if ( IsPythonScript() )
       {
         SetIconImage("DefaultScript.png");
+      }
+      else if ( IsGame() )
+      {
+        SetIconImage("DefaultGame.png");
       }
       else
       {
