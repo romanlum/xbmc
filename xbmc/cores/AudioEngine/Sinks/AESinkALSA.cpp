63db99c0de130fdf9cd8e79cecec4e0938d6027e
Garrett Brown <garbearucla@gmail.com>
2013-03-10 12:54:53 -0700

    Audio: Fixed delay on ALSA devices, and disabled audio delay setting in game settings

--- xbmc/cores/AudioEngine/Sinks/AESinkALSA.cpp
+++ xbmc/cores/AudioEngine/Sinks/AESinkALSA.cpp
@@ -33,6 +33,7 @@
 #include "utils/MathUtils.h"
 #include "threads/SingleLock.h"
 #include "settings/GUISettings.h"
+#include "Application.h"
 #if defined(HAS_AMLPLAYER)
 #include "cores/amlplayer/AMLUtils.h"
 #endif
@@ -332,6 +333,30 @@ bool CAESinkALSA::InitializeHW(AEAudioFormat &format)
 
   snd_pcm_uframes_t periodSize, bufferSize;
   snd_pcm_hw_params_get_buffer_size_max(hw_params, &bufferSize);
+  snd_pcm_hw_params_get_period_size_max(hw_params, &periodSize, NULL);
+
+  if (!g_application.IsPlayingGame())
+  {
+  /* 
+   We want to make sure, that we have max 200 ms Buffer with 
+   a periodSize of approx 50 ms. Choosing a higher bufferSize
+   will cause problems with menu sounds. Buffer will be increased
+   after those are fixed.
+  */
+  periodSize  = std::min(periodSize, (snd_pcm_uframes_t) sampleRate / 20);
+  bufferSize  = std::min(bufferSize, (snd_pcm_uframes_t) sampleRate / 5);
+  }
+  else
+  {
+    periodSize  = std::min(periodSize, (snd_pcm_uframes_t) sampleRate / 40); // 25ms
+    bufferSize  = std::min(bufferSize, (snd_pcm_uframes_t) periodSize * 2);  // 50ms
+  }
+
+  /* 
+   According to upstream we should set buffer size first - so make sure it is always at least
+   4x period size to not get underruns (some systems seem to have issues with only 2 periods)
+  */
+  periodSize = std::min(periodSize, bufferSize / 4);
 
   bufferSize  = std::min(bufferSize, (snd_pcm_uframes_t)8192);
   periodSize  = bufferSize / ALSA_PERIODS;
